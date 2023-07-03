d347a35096ae71abad3d84c6f99f5d16d0255a0b
Kacper Stasik <stasikkacper1@gmail.com>
2023-07-03 18:54:23 -0500

    Loop and Extended Lines support

--- cogs/train.py
+++ cogs/train.py
@@ -4,80 +4,127 @@ from discord.ui import Select, View
 from discord.ext import commands
 import requests
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
+import math
 
 class Train(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.lines = {"Orange": {"desc": "Midway - Loop", "emoji": "🟠"}, 
-        "Red": {"desc": "Howard - 95th/Dan Ryan", "emoji": "🔴"}, 
-        "Green": {"desc": "Harlem - Ashland/63rd", "emoji": "🟢"}, 
-        "Blue": {"desc": "O'hare - Forest Park", "emoji": "🔵"}, 
-        "Pink": {"desc": "54th/Cermak - Loop", "emoji": "🌸"}, 
-        "Purple": {"desc": "Linden - Loop", "emoji": "🟣"}, 
-        "Brown": {"desc": "Kimball - Loop", "emoji": "🟤"},
-        "Yellow": {"desc": "Skokie - Howard", "emoji": "🟡"}}
+        self.lines = {"Orange": {"desc": "Midway - Loop", "emoji": "🟠", "rt": "Org"}, 
+        "Red": {"desc": "Howard - 95th/Dan Ryan", "emoji": "🔴", "rt": "Red"}, 
+        "Green": {"desc": "Harlem - Ashland/63rd", "emoji": "🟢", "rt": "G"}, 
+        "Blue": {"desc": "O'hare - Forest Park", "emoji": "🔵", "rt": "Blue"}, 
+        "Pink": {"desc": "54th/Cermak - Loop", "emoji": "🌸", "rt": "Pink"}, 
+        "Purple": {"desc": "Linden - Loop", "emoji": "🟣", "rt": "P"}, 
+        "Brown": {"desc": "Kimball - Loop", "emoji": "🟤", "rt": "Brn"},
+        "Yellow": {"desc": "Skokie - Howard", "emoji": "🟡", "rt": "Y"}}
         self.data = json.load(open('./stations.json', 'r'))
 
-    def time_converter(timestamp):
-        pass
+    def normal_round(self, n):
+        if n - math.floor(n) < 0.5:
+            return math.floor(n)
+        return math.ceil(n)
+
+    def arrival_string(self, body: json, i: int, string: str):
+        time = datetime.fromisoformat(body[i]["arrT"])
+        current_time = datetime.now()
+        minute_til_arrival = time - current_time
+        status = None
+        if body[i]["isApp"] == "1":
+            status = "DUE"
+        elif body[i]["isDly"] == "1":
+            status = "DELAYED"
+        string += f'*{str(self.normal_round(minute_til_arrival.total_seconds()/60))+"m" if status == None else status} {"📶" if body[i]["isSch"] == "0" else "🕒"}* '
+        return string
+
+    def get_arrival_times(self, params, rt: str):
+        requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'HIGH:!DH:!aNULL'
+        response = requests.get(f'https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?', params=params)
+        resp_result = response.json()
+        body = resp_result["ctatt"]["eta"]
+        trDr1, trDr5 = "", ""
+        string = f'**{body[0]["staNm"]}**\n*Displaying {rt["emoji"]} arrival times*\n'
+        for i in range(len(body)):
+            if body[i]["trDr"] == "1" and body[i]["rt"] == rt["rt"]:
+                if trDr1 == "":
+                    trDr1 += f'**Service towards {body[i]["destNm"]}**\n'
+                trDr1 = self.arrival_string(body, i, trDr1)
+            elif body[i]["trDr"] == "5" and body[i]["rt"] == rt["rt"]: 
+                if trDr5 == "":
+                    trDr5 += f'\n**Service towards {body[i]["destNm"]}**\n'
+                trDr5 = self.arrival_string(body, i, trDr5)
+        string += (trDr1 + trDr5)
+        return string
 
     @app_commands.command(name="train", description="this is a train command")
     async def train(self, interaction: discord.Interaction):
         try:
-            selectLines = Select(
-                placeholder="Please select a line",
-                options=[discord.SelectOption(label=line, description=info['desc'], emoji=info['emoji']) for line, info in self.lines.items()])
-            
             async def line_callback(interaction):
-        
+
                 async def get_arrivals(interaction):
+                    if selectStations.values[0] == "Loop": 
+                        await choose_station(interaction)
+                        return
                     params = {
                         "key": os.getenv("CTA_TRAIN_TOKEN"),
                         "mapid": self.data[selectLines.values[0]][selectStations.values[0]],
                         "outputType": "JSON"
                     }
-                    requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'HIGH:!DH:!aNULL'
-                    response = requests.get(f'https://lapi.transitchicago.com/api/1.0/ttarrivals.aspx?', params=params)
-                    resp_result = response.json()
-                    body = resp_result["ctatt"]["eta"]
-                    trDr1, trDr5 = "", ""
-                    stringtosend = f'**{body[0]["staNm"]}**\n\n'
+                    stringtosend = self.get_arrival_times(params, rt=self.lines[selectLines.values[0]])
+                    await interaction.response.edit_message(content=stringtosend, view=None)
 
-                    for i in range(len(body)):
-                        if body[i]["trDr"] == "1":
-                            if trDr1 == "":
-                                trDr1 += f'**{body[i]["stpDe"]}**\n'
-                            time = datetime.fromisoformat(body[i]["arrT"]).time().minute
-                            current_time = datetime.now().time().minute
-                            minute_til_arrival = time - current_time
-                            trDr1 += f'*{str(minute_til_arrival)+"m" if body[i]["isApp"] == "0" else "DUE"} {"📶" if body[i]["isSch"] == "0" else "🕒"}* '
-                        else: 
-                            if trDr5 == "":
-                                trDr5 += f'\n**{body[i]["stpDe"]}**\n'
-                            time = datetime.fromisoformat(body[i]["arrT"]).time().minute
-                            current_time = datetime.now().time().minute
-                            minute_til_arrival = time - current_time
-                            trDr5 += f'*{str(minute_til_arrival)+"m" if body[i]["isApp"] == "0" else "DUE"} {"📶" if body[i]["isSch"] == "0" else "🕒"}* '
+                async def choose_station(interaction):
 
-                    stringtosend += (trDr1 + trDr5)
-                    await interaction.response.send_message(stringtosend)
+                    async def get_arrivalsExt(interaction):
+                        params = {
+                            "key": os.getenv("CTA_TRAIN_TOKEN"),
+                            "mapid": self.data[selectLines.values[0]][selectBranch.values[0] if selectLines.values[0] in extended_lines else selectStations.values[0]][selectExtStations.values[0]],
+                            "outputType": "JSON"
+                        }
+                        stringtosend = self.get_arrival_times(params, rt=self.lines[selectLines.values[0]])
+                        await interaction.response.edit_message(content=stringtosend, view=None)
 
+                    selectExtStations = Select(
+                        placeholder="Select a station",
+                        options=[discord.SelectOption(label=station) for station in self.data[selectLines.values[0]][selectBranch.values[0] if selectLines.values[0] in extended_lines else selectStations.values[0]]]
+                    )
+                    selectExtStations.callback = get_arrivalsExt
+                    viewStations = View()
+                    viewStations.add_item(selectExtStations)
+                    await interaction.response.edit_message(content=f"Selected line: `{selectLines.values[0]}`\nWhich station are you at?", view=viewStations)
+
+                # Two options can occur:     
                 selectStations = Select(
                     placeholder="Select a station",
                     options=[discord.SelectOption(label=station) for station in self.data[selectLines.values[0]]]
                 )
+                selectBranch = Select(
+                    placeholder="Select a branch",
+                    options=[discord.SelectOption(label=branch) for branch in self.data[selectLines.values[0]]]
+                )
+
+                extended_lines = ["Red", "Green", "Blue"]
+
                 selectStations.callback = get_arrivals
-                viewStations = View()
+                selectBranch.callback = choose_station
+
+                viewStations, viewBranch = View(), View()
+
                 viewStations.add_item(selectStations)
-                await interaction.response.send_message(f"Selected line: `{selectLines.values[0]}`\nWhich station are you at?", view=viewStations)
+                viewBranch.add_item(selectBranch)
 
+                if selectLines.values[0] not in extended_lines:
+                    await interaction.response.edit_message(content=f"Selected line: `{selectLines.values[0]}`\nWhich station are you at?", view=viewStations)
+                else:
+                    await interaction.response.edit_message(content=f"Selected branch: `{selectLines.values[0]}`\nWhich branch are you on?", view=viewBranch)
 
+            selectLines = Select(
+                placeholder="Please select a line",
+                options=[discord.SelectOption(label=line, description=info['desc'], emoji=info['emoji']) for line, info in self.lines.items()])
             selectLines.callback = line_callback
             viewLines = View()
             viewLines.add_item(selectLines)
-
             await interaction.response.send_message("Hello! Which line are you taking?", view=viewLines)
 
         except Exception as e:
